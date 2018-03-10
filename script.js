42ab2439a286156b08b8d8dbbc63e5c99ae10bab
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2018-03-10 13:21:44 +1100

    changed so smooth transition of second hand still works

--- script.js
+++ script.js
@@ -2,23 +2,29 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+var date = new Date()
+console.log(date)
+// Use get method to extract details from date, eg hr, min, seconds
+let hr = date.getHours()
+let min = date.getMinutes()
+let sec = date.getSeconds()
+console.log("Hour: " + hr + " Minute: " + min + " Seconds: " + sec )
+
+// Will contain the degrees we want arms to move
+//calculation for hour hand
+let hrPosition = (hr*360/12) + (min*(360/60)/12)
+// calculation for minute hand 360 degress 60 is minutes then sec broken down into segments
+let minPosition = (min*360/60)+(sec*(360/60)/60)
+// calculation for second hand 
+let secPosition = sec*360/60
 
 function runTheClock() {
-    var date = new Date()
-    console.log(date)
-    // Use get method to extract details from date, eg hr, min, seconds
-    let hr = date.getHours()
-    let min = date.getMinutes()
-    let sec = date.getSeconds()
-    console.log("Hour: " + hr + " Minute: " + min + " Seconds: " + sec )
+    hrPosition = hrPosition + (3/360)
+                            //one sixtieth degree of the second hand
+    minPosition = minPosition + (6/60)
+                            // degrees per second
+    secPosition = secPosition + 6
 
-    // Will contain the degrees we want arms to move
-    //calculation for hour hand
-    let hrPosition = (hr*360/12) + (min*(360/60)/12)
-    // calculation for minute hand 360 degress 60 is minutes then sec broken down into segments
-    let minPosition = (min*360/60)+(sec*(360/60)/60)
-    // calculation for second hand 
-    let secPosition = sec*360/60
 
     // Degrees for inline styles for transform on each of the objects
     HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"
