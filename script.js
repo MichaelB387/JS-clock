8c4eeabaa0541c3674e356d0c6b7d9e13862de2d
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2018-03-10 12:47:19 +1100

    moved Position lets for correct structure

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-// Will contain the degrees we want arms to move
-let hrPosition = 20
-let minPosition = 130
-let secPosition = 267
+
 
 var date = new Date()
 // Use get method to extract details from date, eg hr, min, seconds
 let hr = date.getHours()
 let min = date.getMinutes()
 let sec = date.getSeconds()
+
+// Will contain the degrees we want arms to move
+let hrPosition = 20
+let minPosition = 130
+let secPosition = 267
 
 // Degrees for inline styles for transform on each of the objects
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"
