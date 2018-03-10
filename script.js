e02977fd0641f878e887e36b6e6108b8a7f6b290
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2018-03-10 13:00:41 +1100

    set up hands with calculation position of current time - static

--- script.js
+++ script.js
@@ -5,15 +5,20 @@ const SECONDHAND = document.querySelector("#second");
 
 
 var date = new Date()
+console.log(date)
 // Use get method to extract details from date, eg hr, min, seconds
 let hr = date.getHours()
 let min = date.getMinutes()
 let sec = date.getSeconds()
+console.log("Hour: " + hr + " Minute: " + min + " Seconds: " + sec )
 
 // Will contain the degrees we want arms to move
-let hrPosition = 20
-let minPosition = 130
-let secPosition = 267
+//calculation for hour hand
+let hrPosition = (hr*360/12) + (min*(360/60)/12)
+// calculation for minute hand 360 degress 60 is minutes then sec broken down into segments
+let minPosition = (min*360/60)+(sec*(360/60)/60)
+// calculation for second hand 
+let secPosition = sec*360/60
 
 // Degrees for inline styles for transform on each of the objects
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"
