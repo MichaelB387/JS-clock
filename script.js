94d24012e7b9195caae0a0b89ed5646823b826be
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2018-03-10 12:42:04 +1100

    initial setup of clock hand positions

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+// Will contain the degrees we want arms to move
+let hrPosition = 20
+let minPosition = 130
+let secPosition = 267
+
+// Degrees for inline styles for transform on each of the objects
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)"
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)"
